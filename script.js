1a6939eed65fc0d574854b6c2d9fa27bd9df4a5c
YossecSA <yosec.arratea22@gmail.com>
2024-11-09 00:15:20 -0500

    agregar y eliminar clases de css para las etiquetas

--- script.js
+++ script.js
@@ -4,20 +4,29 @@ const botonEnFoque = document.querySelector(".app__card-button--enfoque ");
 const botonLargo = document.querySelector(".app__card-button--largo")
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title')
+const listaBotones = document.querySelectorAll('.app__card-button')
 
 btnCorto.addEventListener("click", () => {
     cambiarContexto("descanso-corto");
+    btnCorto.classList.add("active");
 });
 
 botonEnFoque.addEventListener("click", () => {
     cambiarContexto("enfoque");
+    botonEnFoque.classList.add("active");
 });
 
 botonLargo.addEventListener("click", () => {
     cambiarContexto("descanso-largo");
+    botonLargo.classList.add("active");
 });
 
 function cambiarContexto(contexto){
+
+    listaBotones.forEach( (contexto) => {
+        contexto.classList.remove("active")
+    })
+
     html.setAttribute("data-contexto", contexto);
     banner.setAttribute('src', `./imagenes/${contexto}.png`)
 
